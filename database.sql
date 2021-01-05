c87cccccfdd6a00082664e79174c2358eb3d89bd
Joshua Vedane <jvedane@gmail.com>
2021-01-05 16:37:45 -0600

    inserted dummy data into the database tables

--- database.sql
+++ database.sql
@@ -12,3 +12,17 @@ CREATE TABLE "song"(
 	"length" VARCHAR(10) NOT NULL, 
 	"released" DATE	
 );
+
+-- Existing Data
+INSERT INTO "artists" ("name", "birthdate")
+VALUES 
+	('Ella Fitzgerald', '04-25-1917'),
+	('Dave Brubeck', '12-06-1920'),
+	('Miles Davis', '05-26-1926'),
+	('Esperanza Spalding', '10-18-1984');
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES 
+	('Take Five','5:24','1959-09-29'),
+	('So What','9:22','1959-08-17'),
+	('Black Gold','5:17','2012-02-01');
