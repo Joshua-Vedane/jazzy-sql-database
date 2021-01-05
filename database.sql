59af52934e4c72465aff91799b4bf9cb7d273616
Joshua Vedane <jvedane@gmail.com>
2021-01-05 16:25:27 -0600

    created databases artist and song

--- database.sql
+++ database.sql
@@ -1,5 +1,14 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+--  database name jazzy_sql
+
+CREATE TABLE "artists"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"birthdate" date
+);
+
+CREATE TABLE "song"(
+	"id" SERIAL PRIMARY KEY, 
+	"title" VARCHAR(255) NOT NULL,
+	"length" VARCHAR(10) NOT NULL, 
+	"released" DATE	
 );
